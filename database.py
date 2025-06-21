cd07b87addbbadd1d2e0f2e2e872c0a87024b2eb
Shuvrajyoti <shuvra458@gmail.com>
2025-06-21 22:50:14 +0530

    clever cloud database updated url

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:shuvra@localhost:3306/wallet_db"
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://u4qvosefaohyziif:KHwFd8dn5gRkFh9cwGlg@bn885c1u5wzfubmclp63-mysql.services.clever-cloud.com:3306/bn885c1u5wzfubmclp63"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
