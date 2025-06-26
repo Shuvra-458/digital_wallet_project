f147776b61befa86b4a920bf6920b8956ed0c147
Shuvrajyoti <shuvra458@gmail.com>
2025-06-26 12:46:00 +0530

    Update currency.py

--- currency.py
+++ currency.py
@@ -1,6 +1,10 @@
 import requests
+import os
+from dotenv import load_dotenv
 
-API_KEY = "cur_live_6YZTLrFooylzf4CDpb7H7RpmiMcSxx3se2OAfwSX"  
+load_dotenv()
+
+API_KEY = os.getenv("API_KEY")
 
 def convert_inr_to(inr_amount: float, target_currency: str) -> float:
     url = f"https://api.currencyapi.com/v3/latest?apikey={API_KEY}&base_currency=INR&currencies={target_currency}"
